28e9a574d2dee603aba0f8bb7cebe59649422aeb
KuhnFinn <finqun@web.de>
2019-12-18 16:08:39 +0100

    add dropdown menu

--- lib/Routes/CreateSchedule.dart
+++ lib/Routes/CreateSchedule.dart
@@ -2,7 +2,72 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/data/Text.dart';
 
-class CreateSchedule extends StatelessWidget {
+class CreateSchedule extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _initState();
+}
+
+enum sports { Fussball, Faustball }
+
+class _initState extends State<CreateSchedule> {
+  List<String> _sports = ['Fussball', 'Faustball'];
+  String _seleectedSport;
+  var _sport;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(str_titelCreateSchedule),
+      ),
+      body: Column(children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: TextFormField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Titel',
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: TextFormField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Sportart',
+            ),
+          ),
+        ),
+
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: DropdownButtonFormField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+            ),
+            value: _seleectedSport,
+            items: _sports
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: (newValue) {
+              setState(() {
+                _seleectedSport = newValue;
+              });
+            },
+            hint: Text("Sportart"),
+          ),
+        ),
+      ]),
+    );
+  }
+}
+
+class CreateSchedule2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -29,20 +94,7 @@ class CreateSchedule extends StatelessWidget {
             ),
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: DropdownButton<String>(
-            icon: Icon(Icons.arrow_drop_down),
-            value: 'Drei',
-            items: <String>['One', 'Two', 'Free', 'Four']
-                .map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-          ),
-        ),
+        Padding(padding: const EdgeInsets.all(4.0)),
       ]),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
       title: str_titel,
       theme: ThemeData(
         primarySwatch: Colors.green,
-
       ),
       home: MyHomePage(title: str_titel),
     );
@@ -22,8 +21,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-
-
   final String title;
 
   @override
@@ -31,59 +28,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    Schedule tab1 =new Schedule("Spieltga 2", new DateTime(2019,12,9,12,10), new DateTime(2019,3,30),"Fußball");
+    Schedule tab1 = new Schedule(
+        "Spieltga 2",
+        new DateTime(2019, 12, 9, 12, 10),
+        new DateTime(2019, 3, 30),
+        "Fußball");
 
     return Scaffold(
       appBar: AppBar(
-
         title: Text(widget.title),
         leading: Icon(Icons.menu),
       ),
       body: Column(
-
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Card(
-
-              child: Column(
-                children: <Widget>[
-                  Image.asset('assets/images/Fußball_klein.png'),
-                  ListTile(
-
-                    title:  Text("Spielplan 1"),
-                    subtitle: Text("03.03.2019"),
-                  ),
-                ],
-              ),
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Card(
+            child: Column(
+              children: <Widget>[
+                Image.asset('assets/images/Fußball_klein.png'),
+                ListTile(
+                  title: Text("Spielplan 1"),
+                  subtitle: Text("03.03.2019"),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: tab1.generateCard()
-            ),
-            Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: tab1.generateCard()
-            ),
-
-          ],
-        ),
+          ),
+          Padding(
+              padding: const EdgeInsets.all(4.0), child: tab1.generateCard()),
+          Padding(
+              padding: const EdgeInsets.all(4.0), child: tab1.generateCard()),
+        ],
+      ),
 
       floatingActionButton: FloatingActionButton(
-        onPressed: () {Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => CreateSchedule()),
-        );
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => CreateSchedule()),
+          );
         },
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
